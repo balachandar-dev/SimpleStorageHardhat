f311232a6ee8ed32048f037f24bfc06c3d7921d3
Balachandar Mutharasu <balachandar.mutharasu@gmail.com>
2025-02-12 23:52:41 +0530

    Verifying a contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("@nomicfoundation/hardhat-ethers")
+require("@nomicfoundation/hardhat-verify")
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,4 +18,7 @@ module.exports = {
         },
     },
     solidity: "0.8.28",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 }
--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -1,14 +1,34 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
-    const simpleStorageFactory = await ethers.getContractFactory(
-        "SimpleStorage"
-    )
-    console.log("deploying")
+    const simpleStorageFactory =
+        await ethers.getContractFactory("SimpleStorage")
+    // console.log("deploying")
     const simpleStorage = await simpleStorageFactory.deploy()
     await simpleStorage.waitForDeployment()
 
-    console.log("deployed at", simpleStorage.target)
+    // console.log("deployed at", simpleStorage.target)
+    console.log(network.config)
+
+    if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
+        simpleStorage.deploymentTransaction(6)
+        await verify(simpleStorage.target, [])
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("verifying")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified")
+        }
+        console.error("verification failed", e)
+    }
 }
 
 main()
